be2e0cc793dded993d9d19f03b8782d4db7ddec1
spookeyy <pangasmeshack@gmail.com>
2024-03-31 09:51:09 +0300

    event listeners

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert(){
+    alert('I was clicked');
+};
+input.addEventListener('click', clickAlert)
 }
+
+// const input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+
